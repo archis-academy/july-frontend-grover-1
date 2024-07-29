443d18be0777f680935d51cb6bb0a6ba2dfabf14
Akile  Nisa Öksüz <116032825+Akile-Nisa-Oksuz@users.noreply.github.com>
2024-07-29 12:43:27 +0300

    [JU-2] header-section burger menü düzeltildi ve açılan menü simgesi düzeltildi

--- app.js
+++ app.js
@@ -5,9 +5,19 @@ let isMenuOpen = false;
 toggleMenu.addEventListener("click", function () {
   if (!isMenuOpen) {
     navBarMenu.style.display = "flex";
-    isMenuOpen = !isMenuOpen;
+    isMenuOpen = true;
   } else {
     navBarMenu.style.display = "none";
-    isMenuOpen = !isMenuOpen;
+    isMenuOpen = false;
+  }
+});
+
+window.addEventListener("resize", function () {
+  if (window.innerWidth > 1300) {
+    navBarMenu.style.display = "none";
+    isMenuOpen = false; 
+  } else {
+    navBarMenu.style.display = "none";
+    isMenuOpen = false;
   }
 });
